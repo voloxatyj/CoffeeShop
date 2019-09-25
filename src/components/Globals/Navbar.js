187b83b22082136c8665593504d5a074e4f4f75c
abc <abc@example.com>
2019-09-25 18:34:17 +0300

    Navbar added as well as basic css

---
+++ src/components/Globals/Navbar.js
@@ -0,0 +1,69 @@
+import React, { Component } from "react"
+import { Link } from "gatsby"
+import logo from "../../images/iconfinder_007_-_Coffee_Cup_3817232.svg"
+import { FaCartArrowDown } from "react-icons/fa"
+
+export default class Navbar extends Component {
+  state = {
+    navbarOpen: false,
+    css: "collapse navbar-collapse d-none",
+    links: [
+      {
+        id: 1,
+        path: "/",
+        text: "home",
+      },
+      {
+        id: 2,
+        path: "/about",
+        text: "about",
+      },
+    ],
+  }
+  navbarHandler = () => {
+    this.state.navbarOpen
+      ? this.setState({
+          navbarOpen: false,
+          css: "collapse navbar-collapse",
+        })
+      : this.setState({
+          navbarOpen: true,
+          css: "collapse navbar-collapse show",
+        })
+  }
+  render() {
+    return (
+      <nav className="navbar navbar-expand-sm bg-light navbar-light">
+        <Link to="/" className="navbar-brand">
+          <img src={logo} alt="logo" />
+          {/* https://www.iconfinder.com/icons/3817232/coffee_cup_drink_free_hipster_on_trend_icon
+        Creative Commons (Attribution 3.0 Unported);
+       https://www.iconfinder.com/webalys */}
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          onClick={this.navbarHandler}
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className={this.state.css}>
+          <ul className="navbar-nav mx-auto">
+            {this.state.links.map(item => {
+              return (
+                <li key={item.id} className="nav-item">
+                  <Link to={item.path} className="nav-link text-capitalize">
+                    {item.text}
+                  </Link>
+                </li>
+              )
+            })}
+            <li className="nav-item ml-sm-5">
+              <FaCartArrowDown className="cart-icon" />
+            </li>
+          </ul>
+        </div>
+      </nav>
+    )
+  }
+}
